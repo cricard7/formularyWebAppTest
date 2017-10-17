88f7d3f5f2de2c9851b38f0be5ea3baa6e2921ab
cricard7 <chad_ricard@hotmail.com>
2017-10-17 09:55:48 -0500

    intial setup. Added vue.js vue-material and default stylesheets. Hooked up vue to index.html and initialized vue-material.

---
+++ script/app.js
@@ -0,0 +1,42 @@
+(function () {
+
+	
+	Vue.use(VueMaterial);
+	
+	
+	var formApp = new Vue({
+
+		el: '#formularyWebApp',
+		data: {
+			msg: "Hello"
+		},
+
+		// created lifecycle hook gets called automatically
+		created: function () {
+
+
+		},
+
+		methods: {
+
+		},
+
+		computed: {
+
+
+		},
+
+		watch: {
+
+		},
+
+		// local components defined before myApp vue instance
+		components: {
+
+		}
+
+	})
+
+
+
+}());
